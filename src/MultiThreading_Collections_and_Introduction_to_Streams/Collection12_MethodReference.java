15e068437f112958352b7a2733854e71a5df5cff
Rushikesh Dhakulkar <rushi53520dhakulkar@gmail.com>
2026-06-12 11:01:55 +0530

    Added New_Concepts examples

--- src/MultiThreading_Collections_and_Introduction_to_Streams/Collection12_MethodReference.java
+++ src/MultiThreading_Collections_and_Introduction_to_Streams/Collection12_MethodReference.java
@@ -1,4 +1,18 @@
 package MultiThreading_Collections_and_Introduction_to_Streams;
 
-public class Collection12_MethodReference {
+import java.util.Arrays;
+import java.util.List;
+
+public class Collection12_MethodReference
+{
+    public static void main(String[] args) {
+        List<String> names= Arrays.asList("Aryan","Nayan","Kamala","Nitya");
+
+        List<String> UName=names.stream().map(String::toUpperCase).toList();
+        UName.forEach(System.out::println);
+
+
+
+    }
 }
+
